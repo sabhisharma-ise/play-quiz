e42aea7e3a3429e670103f979d0c0c15ba5bdb48
sabhisharma-ise <2021is_sabhisharma_b@nie.ac.in>
2023-02-04 21:34:06 +0530

    Adding comments

--- js/indexQuiz.js
+++ js/indexQuiz.js
@@ -1,8 +1,11 @@
+
 var qno = [1, 2, 3, 4];
+
 var question = ["Which digital payment company has launched unified QR code for merchants for unlimited payments?", 
 "Read along, that was in news recently, is an application of which multi-national company?", 
 "The DOGE-1 Mission to the Moon will be launched by which company in 2022?", 
 "Which famous technology company has launched a short music videos application called Collab?"];
+
 var allAns = [["PhonePe", "Paytm", "Amazon Pay", "Google Pay"], 
 ["Amazon", "Facebook", "Microsoft", "Google"], 
 ["SpaceX", "ISRO", "NASA", "Roscosmos"], 
@@ -16,6 +19,7 @@ var j = 0;
 var x = document.querySelectorAll(".ans-btn");
 for (i = 0; i < x.length; i++) {
 
+    // Event listener for a button when a user clicks
     x[i].addEventListener('click', function() {
         if (this.textContent === rightAns[j]) {
             score++;
@@ -31,6 +35,8 @@ for (i = 0; i < x.length; i++) {
 
     })
 }
+
+// Function for stepping into the next question after user clicks an option
 function next() {
     document.querySelector('.current-q').textContent=(question[j]);
     document.querySelector('#qno').textContent=(qno[j]);
@@ -39,16 +45,15 @@ function next() {
     } 
 }
 
+// Function call when the quiz ends
 function scorecard() {
-    // for(i=0;i<x.length;i++){
-    //     document.querySelectorAll('.ans-btn')[i].addEventListener('click', function() {
-    //         window.location.href = "score.html";
-    //         // document.querySelector("#result").textContent = score;
-    //     });
-    // }
+
+    // Making the content NULL after the quiz ends
     document.querySelector("h1").innerHTML = "";
     document.querySelector("h3").innerHTML = "";
     document.querySelector(".ans-btn-div").innerHTML = "";
+
+    // Making the Hidden Button visible after end of quiz
     document.querySelector('.show-res').classList.add("score-visible");
     document.querySelector('.show-res').addEventListener('click',function(){
         document.querySelector('button').innerHTML = 'You scored ' + score + ' out of 4';
